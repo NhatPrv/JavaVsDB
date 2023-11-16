314a9dfbbfeafe3342bc5e26be7eb511b1417b4f
NhatPrv <torikun2005@gmail.com>
2023-11-17 01:20:10 +0700

    Java connect to DataBase

---
+++ src/database/JDBCUtil.java
@@ -0,0 +1,34 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCUtil {
+    public static Connection getConnection(){
+
+        Connection conn = null;
+        // dang ki driver
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            // Tạo URL kết nối và thông tin đăng nhập
+
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=DemoJDBC";
+            String username = "sa";
+            String password = "@37Nholldz";
+
+            // Lấy kết nối
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+    public static void closeConnection(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
